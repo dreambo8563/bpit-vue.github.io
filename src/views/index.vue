285f6e41838d2e52575a91fcc211c625e3882eee
dreambo8563 <dreambo8563@outlook.com>
2018-09-30 13:45:09 +0800

    feat(add index desc):

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
     <headebar></headebar>
-    <h1 class="main-center">bpit/vue</h1>
+    <br>
+    <div class="main-center title">BPIT/VUE</div>
+    <br>
     <section class="main-logo main-center">
       <img width="200" src="https://cli.vuejs.org/favicon.png" alt="">
         </section>
       <section class="main-content">
         <h4> What is bpit/vue?</h4>
-        What is Lorem Ipsum?
-        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
-
-        <br>
-        <h4>What we focus?</h4>
-        <br>
-        <h4>Why we need it?</h4>
-        Why do we use it?
-        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
-
-        Where does it come from?
-        Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
-
-        The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.
+        <p><i>bpit/vue</i> is not a component UI kit, we didn't have the consistent theme for these components, it's just a collection of effects components. <br></p>
+          <p>bpit/vue is a good place for beginners to learn <b>How to create their UI Kits</b>. Now, it's just a showcase of the effects I can gather from the Internet (codepen, github...) and transfered to vue `things`</p>
+          <br>
+          <h4>What we focus?</h4>
+          <p>We focus on the effects of these components other than functionailties. That's why it's not a ui kit.</p>
+          <p>We take more time on css part and hope to make it more general and easy to use, so we encapsulate them into vue component/directives.</p>
+          <br>
+          <h4>Why we need it?</h4>
+          <p>There are so many resources on the Internet, we can make use of them to save time and to be more efficient. <i>bpit/vue</i> collects them, encapsulates them, imporves them. </p>
+          <u>Install easy</u> && <u>Import on demand</u> && <u>Everything in one place</u>
       </section>
       <br>
   </div>
@@ -40,5 +38,51 @@ export default {
   max-width: 80%;
   font-size: 16px;
   margin: 0 auto;
+}
+.title {
+  width: 250px;
+  font-size: 28px;
+  font-weight: 700;
+  font-style: italic;
+  color: white;
+  position: relative;
+  letter-spacing: 0.15em;
+  margin: 0 auto;
+  padding: 1rem 2.5rem;
+  background: transparent;
+  outline: none;
+  font-size: 28px;
+  transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55) 0.15s;
+}
+.title::before {
+  top: 0.5rem;
+  left: 0.35rem;
+  transform: skewx(-10deg);
+  height: 100%;
+  width: 100%;
+  border-radius: 0;
+  content: "";
+  position: absolute;
+  transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  z-index: -2;
+  background-color: #c92918;
+}
+.title::after {
+  left: 0rem;
+  top: 0;
+  transform: skewx(-10deg);
+  height: 100%;
+  width: 100%;
+  border-radius: 0;
+  animation: none;
+  content: "";
+  position: absolute;
+  transition: all 0.5s cubic-bezier(0.68, -0.55, 0.265, 1.55);
+  z-index: -2;
+  background-color: #e74c3c;
+}
+u {
+  font-size: 18px;
+  font-weight: 500;
 }
 </style>
